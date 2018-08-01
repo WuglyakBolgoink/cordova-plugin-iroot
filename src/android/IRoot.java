ea78a86493c45591bdbe983d9e97f94ae665683b
Ali Elderov <elderov@fundsaccess.com>
2018-08-01 09:53:46 +0200

    💊 cleanning in src/android/IRoot.java

--- src/android/IRoot.java
+++ src/android/IRoot.java
@@ -12,48 +12,92 @@ import java.io.File;
 
 import com.scottyab.rootbeer.RootBeer;
 
+import org.apache.cordova.LOG;
+
 
 /**
  * Detect weather device is rooted or not
+ *
  * @author trykov
+ * @author Ali Elderov
  */
 public class IRoot extends CordovaPlugin {
+    private static final String LOG_TAG = "[IRoot]";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("isRooted")) {
+        LOG.d(LOG_TAG, "Plugin executed");
+
+        if (action.equals("isRootedRedBeer")) {
+            // Simple check with rootBeer
             try {
-                callbackContext.success(isDeviceRooted() ? 1 : 0);
+                RootBeer rootBeer = new RootBeer(this.cordova.getActivity().getApplicationContext());
+
+                callbackContext.success(rootBeer.isRooted() ? 1 : 0);
+
                 return true;
             } catch (Exception e) {
-                callbackContext.error("Error: N/A");
+                LOG.e(LOG_TAG, "Error", e);
+
+                callbackContext.error("N/A");
+
                 return false;
             }
-        } else if (action.equals("isRootedRedBeer")) {
-                           try {
-                               RootBeer rootBeer = new RootBeer(this.cordova.getActivity().getApplicationContext());
-                               callbackContext.success(rootBeer.isRooted() ? 1 : 0);
-                               return true;
-                           } catch (Exception e) {
-                               callbackContext.error("Error: N/A");
-                               return false;
-                           }
-                       } else if (action.equals("isRootedRedBeerWithoutBusyBoxCheck")) {
-                                                    try {
-                                                        RootBeer rootBeer = new RootBeer(this.cordova.getActivity().getApplicationContext());
-                                                        callbackContext.success(rootBeer.isRootedWithoutBusyBoxCheck() ? 1 : 0);
-                                                        return true;
-                                                    } catch (Exception e) {
-                                                        callbackContext.error("Error: N/A");
-                                                        return false;
-                                                    }
-                                                }
+        } else if (action.equals("isRootedRedBeerWithoutBusyBox")) {
+            // Simple check with rootBeer without BusyBox
+            try {
+                RootBeer rootBeer = new RootBeer(this.cordova.getActivity().getApplicationContext());
+
+                callbackContext.success(rootBeer.isRootedWithoutBusyBoxCheck() ? 1 : 0);
+
+                return true;
+            } catch (Exception e) {
+                LOG.e(LOG_TAG, "Error", e);
+
+                callbackContext.error("N/A");
+
+                return false;
+            }
+        } else if (action.equals("isRooted")) {
+            // Combine simple check and rootBeer. User can activate check with BusyBox -> put argument with TRUE/FALSE.
+            try {
+                RootBeer rootBeer = new RootBeer(this.cordova.getActivity().getApplicationContext());
+
+                boolean without = true;
+
+                try {
+                    without = args.getBoolean(0);
+                    LOG.d(LOG_TAG, "args[0] value is: " + without);
+                } catch (Exception e) {
+                    LOG.e(LOG_TAG, "Error", e);
+
+                    without = true;
+                }
+
+                LOG.d(LOG_TAG, "without: " + without);
+
+                boolean check = isDeviceRooted() || ((without) ? rootBeer.isRootedWithoutBusyBoxCheck() : rootBeer.isRooted());
+                LOG.d(LOG_TAG, "check: " + check);
+
+                callbackContext.success(check ? 1 : 0);
+
+                return true;
+            } catch (Exception e) {
+                LOG.e(LOG_TAG, "Error", e);
+
+                callbackContext.error("N/A");
+
+                return false;
+            }
+        }
+
         return false;
     }
 
     private boolean isDeviceRooted() {
         return checkBuildTags() || checkSuperUserApk() || checkFilePath();
     }
+
     private boolean checkBuildTags() {
         String buildTags = android.os.Build.TAGS;
         return buildTags != null && buildTags.contains("test-keys");
@@ -64,11 +108,23 @@ public class IRoot extends CordovaPlugin {
     }
 
     private boolean checkFilePath() {
-        String[] paths = { "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
-                "/system/bin/failsafe/su", "/data/local/su" };
+        String[] paths = {
+                "/sbin/su",
+                "/system/bin/su",
+                "/system/xbin/su",
+                "/data/local/xbin/su",
+                "/data/local/bin/su",
+                "/system/sd/xbin/su",
+                "/system/bin/failsafe/su",
+                "/data/local/su"
+        };
+
         for (String path : paths) {
-            if (new File(path).exists()) return true;
+            if (new File(path).exists()) {
+                return true;
+            }
         }
+
         return false;
     }
 }
