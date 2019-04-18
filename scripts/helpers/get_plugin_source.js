40bca386f4e64c9491e5acf0e662a5542a16880f
Davide Bizzarri <b1zzu@localhost.localdomain>
2019-04-18 10:11:19 +0200

    Use simple 'require' instant of 'requireCordovaModule' for 'fs' and 'path'

--- scripts/helpers/get_plugin_source.js
+++ scripts/helpers/get_plugin_source.js
@@ -1,8 +1,8 @@
 var helpers = require('./');
+var path = require('path');
+var fs = require('fs');
 
 module.exports = function(platform) {
-    var path = this.requireCordovaModule('path');
-    var fs = this.requireCordovaModule('fs');
     var cordovaUtil = this.requireCordovaModule('cordova-lib/src/cordova/util');
     var projectRoot = cordovaUtil.isCordova();
     var platformPath = path.join(projectRoot, 'platforms', platform);
